61c5411480ba2cff850699efcac38ed606d5de94
Shah Fahad <shah.fahad@tenpearls.com>
2022-03-08 01:07:20 +0500

    Update CORs origins

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors');
+const cors = require("cors");
 
-const db = require('./queries.js');
-const tracker = require('./src/routes/tracker');
+const db = require("./queries.js");
+const tracker = require("./src/routes/tracker");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -16,7 +16,11 @@ app.use(
   })
 );
 
-const whitelist = ["http://localhost:3001", "https://tracker-m11.surge.sh"];
+const whitelist = [
+  "http://localhost:3001",
+  "http://localhost:3001",
+  "https://tracker-m11.surge.sh",
+];
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin || whitelist.indexOf(origin) !== -1) {
@@ -30,22 +34,21 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.get("/", (request, response) => {
-    try {
-        response.json({ info: "Node.js, Express, and Postgres API" });
-    }
-    catch(e){
-        console.log('APP ERROR ', e);
-    }
+  try {
+    response.json({ info: "Node.js, Express, and Postgres API" });
+  } catch (e) {
+    console.log("APP ERROR ", e);
+  }
 });
 
-app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
+app.get("/users", db.getUsers);
+app.get("/users/:id", db.getUserById);
+app.post("/users", db.createUser);
+app.put("/users/:id", db.updateUser);
+app.delete("/users/:id", db.deleteUser);
 
-app.get('/trackers', tracker.getTrackers);
-app.put('/tracker', tracker.setTracker);
+app.get("/trackers", tracker.getTrackers);
+app.put("/tracker", tracker.setTracker);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
